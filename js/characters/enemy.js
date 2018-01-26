aa5ef0a161618bec44410d967f85e56e5b82a123
dawsonsmythe <dawsonsmythe@gmail.com>
2018-01-27 10:42:41 +1300

    Merge conflict fix

--- js/characters/enemy.js
+++ js/characters/enemy.js
@@ -1,19 +1,31 @@
-function createEnemies() { 
+
+function createEnemy() {
+
+    var sprite = balls.create(game.world.randomX, game.world.randomY, 'ball');
+
+    sprite.body.setCircle(16);
+    //sprite.body.collideWorldBounds = false;
+
+    sprite.body.setBodyContactCallback(ship.body, enemyCollide, this);
+    return sprite;
+}
+
+function createEnemies(ship) { 
     balls = game.add.group();
     balls.enableBody = true;
     balls.physicsBodyType = Phaser.Physics.BOX2D;
 
     for (var i = 0; i < 50; i++)
     {
-        var sprite = balls.create(game.world.randomX, game.world.randomY, i % 2 == 0 ? 'ball' : 'firstaid');
-
-        if (i % 2 == 0)
-        {
-            sprite.body.setCircle(16);
-        }
-        else
-        {
-            sprite.body.collideWorldBounds = false;
-        }
+        createEnemy();
     }
+}
+
+function enemyCollide(body1, body2, fixture1, fixture2, begin) {
+    // If 'begin' is true this is a begin contact, otherwise a contact has just ended    ... do something
+    if (begin) {
+        //console.log(Object.getOwnPropertyNames(body1));
+        //console.log(body1.sprite);
+    }
+    
 }
