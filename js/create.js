d6d9373866f99aab85491b8c21b21c0b1ffe9ef2
dawsonsmythe <dawsonsmythe@gmail.com>
2018-01-27 10:14:22 +1300

    Enemy file

--- js/create.js
+++ js/create.js
@@ -6,23 +6,7 @@ function create() {
 
     starfield = game.add.tileSprite(0, 0, 800, 600, 'stars');
 
-    balls = game.add.group();
-    balls.enableBody = true;
-    balls.physicsBodyType = Phaser.Physics.BOX2D;
-
-    for (var i = 0; i < 50; i++)
-    {
-        var sprite = balls.create(game.world.randomX, game.world.randomY, i % 2 == 0 ? 'ball' : 'firstaid');
-
-        if (i % 2 == 0)
-        {
-            sprite.body.setCircle(16);
-        }
-        else
-        {
-            sprite.body.collideWorldBounds = false;
-        }
-    }
+    createEnemies();
 
     ship = game.add.sprite(200, 200, 'ship');
     ship.scale.set(2);
