8aff2b5a0a3c97bc0268480c5128973f3f31e20c
dawsonsmythe <dawsonsmythe@gmail.com>
2018-01-28 10:11:51 +1300

    Merge master

--- js/background.js
+++ js/background.js
@@ -1,11 +1,9 @@
 function createBackground() {
     var background = game.add.sprite(0, 0, 'background');
-    
+
     var workaround = game.add.sprite(0, 0, 'workaround');
     game.physics.box2d.enable([workaround]);
     workaround.body.clearFixtures();
     workaround.body.loadPolygon('physicsData', 'collisionLayer', workaround);
     workaround.body.static = true;
-    // workaround.scale.set(3);
-
 }
