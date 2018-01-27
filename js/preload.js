6daaed347ca6aef4dbf28c6c22e241da7d8fde99
Pedro Worcel <pedro@worcel.com>
2018-01-27 01:23:49 -0500

    Delete assets that are not needed.

--- js/preload.js
+++ js/preload.js
@@ -1,7 +1,4 @@
 function preload() {
-
-    game.load.image('stars', 'assets/misc/starfield.jpg');
-    game.load.image('cell', 'assets/sprites/shinyball.png');
     game.load.image('redCell', 'assets/sprites/redBloodCell.png');
     game.load.image('WBCsmallFront', 'assets/sprites/WBCsmallFront.png');
     game.load.image('WBCsmallBack', 'assets/sprites/WBCsmallBack.png');
@@ -9,7 +6,6 @@ function preload() {
     game.load.image('WBCmedBack', 'assets/sprites/WBCmedBack.png');
     game.load.image('WBClargeFront', 'assets/sprites/WBClargeFront.png');
     game.load.image('WBClargeBack', 'assets/sprites/WBClargeBack.png');
-    game.load.image('firstaid', 'assets/sprites/firstaid.png');
     game.load.spritesheet('virus', 'assets/sprites/virus01.png');
 
     game.load.image('background', 'assets/tilemaps/collisionLayer.png');
