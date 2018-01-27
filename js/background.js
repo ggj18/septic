ea3d3e31bb8c4908f36fc105b486cb3c09f4896b
Pedro Worcel <pedro@worcel.com>
2018-01-26 23:24:36 -0500

    Implement background physics.

---
+++ js/background.js
@@ -0,0 +1,9 @@
+function createBackground() {
+    var background = game.add.sprite(0, 0, 'background');
+
+    var workaround = game.add.sprite(0, 0, 'workaround');
+    game.physics.box2d.enable([workaround]);
+    workaround.body.clearFixtures();
+    workaround.body.loadPolygon('physicsData', 'BGparallax01', workaround);
+    workaround.body.static = true;
+}
--- js/preload.js
+++ js/preload.js
@@ -6,4 +6,8 @@ function preload() {
     game.load.image('firstaid', 'assets/sprites/firstaid.png');
     game.load.spritesheet('virus', 'assets/sprites/virus01.png');
 
+    game.load.image('background', 'assets/tilemaps/BGparallax01.png');
+    game.load.image('workaround', 'assets/sprites/super-workaround.png');
+
+    game.load.physics('physicsData', 'assets/physics/background-physics.json');
 }
