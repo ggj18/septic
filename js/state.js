bdd193d968acc0e3dd2ead7c268fbb0111227404
gabrielamelian <gabrielamelian@gmail.com>
2018-01-28 10:28:42 +1300

    Refactor state, make the game load on splash screen

---
+++ js/state.js
@@ -0,0 +1,42 @@
+var playState = { preload: preload, create: create, update: update, render:
+  render };
+
+var bootState = {
+  create: function () {
+    game.physics.startSystem(Phaser.Physics.BOX2D);
+    space = game.input.keyboard.addKey(Phaser.Keyboard.SPACEBAR);
+    
+    game.state.start('splash');
+  }
+}
+
+var splashState = {
+  create: function () {
+    addText('You can eat things smaller than you, bigger things kill you. Press space to start.');
+    stateCommon(this);
+    game.state.add('play', playState);
+  },
+  update: function () {
+    checkSpacePressed();
+  }
+}
+
+var winState = {
+  create: function () {
+    addText('You WIN!!! Press space to restart.');
+    stateCommon(this);
+  },
+  update: function () {
+    checkSpacePressed();
+  }
+}
+
+var loseState = {
+  create: function () {
+    addText('You LOSE!!! Press space to restart.');
+    stateCommon(this);
+  },
+  update: function () {
+    checkSpacePressed();
+  }
+}
