4fc5c91e249af24b15985b0cd0793db549f50904
Pedro Worcel <pedro@worcel.com>
2018-01-27 17:27:36 -0500

    Interim spawn locations

--- js/render.js
+++ js/render.js
@@ -2,6 +2,6 @@ function render() {
 
     //game.debug.box2dWorld();
     //game.debug.cameraInfo(game.camera, 32, 32);
-    game.debug.pointer( game.input.activePointer );
+//    game.debug.pointer( game.input.activePointer );
     
 }
