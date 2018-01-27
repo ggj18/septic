1f104c47b48a6fd2c2c58df3a0a0a9542a565939
gabrielamelian <gabrielamelian@gmail.com>
2018-01-28 11:07:15 +1300

    Removes un-needed old text rendering functions

--- js/game.js
+++ js/game.js
@@ -6,10 +6,6 @@ function lose() {
   game.state.start('lose')
 }
 
-function addText(text) {
-    game.add.text(0,  0, text, { fill: '#ffffff', font: '14pt Arial' });
-}
-
 function stateCommon(obj) {
     game.stage.backgroundColor = '#000000';
 }
