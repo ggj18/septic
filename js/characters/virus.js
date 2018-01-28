d0dda2ee610facabe6e0b105dd798cdaf33088e2
dawsonsmythe <dawsonsmythe@gmail.com>
2018-01-28 15:20:22 +1300

    Change white cell agro radius

--- js/characters/virus.js
+++ js/characters/virus.js
@@ -108,23 +108,6 @@ function updateVirusSize(virus)
     var zoomTarget = lerp(1.0, 0.0, virus.s_size / 100);
     setCameraZoomTarget(zoomTarget);
 
-    /*
-	// Update game world scale
-	// Size 1 = scale 1. Size 10 = scale 0.4
-    if(virus.s_size <= 3)
-    {
-        game.world.scale.setTo(1.0, 1.0);
-    }
-    else if(virus.s_size <= 6)
-    {
-        game.world.scale.setTo(0.7, 0.7);
-    }
-    else
-    {
-        game.world.scale.setTo(0.4, 0.4);
-    }
-    */
-
     virus.s_doUpdateSize = false;
 }
 
